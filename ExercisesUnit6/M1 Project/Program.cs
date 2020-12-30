1dfc9cfd0743755fcd3887d3490382f3fe17a93e
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-30 12:01:22 +0100

    Fase 1

---
+++ ExercisesUnit6/M1 Project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace M1_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "Miguel Angel";
+            string cognom1 = " Gómez";
+            string cognom2 = " López";
+            int dia = 30;
+            int mes = 12;
+            int any = 2020;
+
+            Console.WriteLine(nom + cognom1 + cognom2);
+            Console.WriteLine(dia +"/"+  mes + "/" +  any);
+        }
+    }
+}
