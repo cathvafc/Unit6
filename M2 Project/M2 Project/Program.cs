7dd4ec9f22279b6addc2a415d8a3dd687a6e505f
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-30 13:33:54 +0100

    Fase 1

---
+++ M2 Project/M2 Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace M2_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] nombre = { 'M', 'I', 'G', 'U', 'E', 'L' };
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine("{0}", nombre[i]);
+            }
+        }
+    }
+}
