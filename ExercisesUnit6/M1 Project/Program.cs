4fed7bfa7f07620a427e1f70ddadfe30d944936c
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-30 12:15:26 +0100

    Fase 2

--- ExercisesUnit6/M1 Project/Program.cs
+++ ExercisesUnit6/M1 Project/Program.cs
@@ -4,7 +4,16 @@ namespace M1_Project
 {
     class Program
     {
+        const int AÑOTRASPASO = 1948;
+        
         static void Main(string[] args)
+        {
+            //Fase1();
+
+            Fase2CalculoAñoTraspaso();
+        }
+
+        public static void Fase1()
         {
             string nom = "Miguel Angel";
             string cognom1 = " Gómez";
@@ -14,7 +23,26 @@ namespace M1_Project
             int any = 2020;
 
             Console.WriteLine(nom + cognom1 + cognom2);
-            Console.WriteLine(dia +"/"+  mes + "/" +  any);
+            Console.WriteLine(dia + "/" + mes + "/" + any);
+        }
+
+        public static void Fase2CalculoAñoTraspaso()
+        {
+            int añoNacimiento = 1996;
+            int cadaCuanto = 4;
+            int cuantosAñosHay = 0;
+     
+            for (int year = AÑOTRASPASO; year <= añoNacimiento;year++) // incluimos el 1996.
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    cuantosAñosHay++;
+                }
+            }
+
+            Console.WriteLine("Entre el {0} y {1} hay {2} años de traspaso.", añoNacimiento, AÑOTRASPASO, cuantosAñosHay);
+
+           
         }
     }
 }
