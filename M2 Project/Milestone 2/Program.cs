ef71a73e3c795898133f94209aa41b310156318c
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-04 09:24:25 +0100

    Milestone 2

---
+++ M2 Project/Milestone 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Milestone_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateLadder();
+        }
+
+        public static void CreateLadder()
+        {
+            Console.WriteLine("Introduce the height of the ladder: ");
+
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            int j =0;
+
+            for (int i = 0; i <= height; i++)
+            {
+                j = 0;
+
+                while( j < i)
+                {
+                    Console.Write("{0}", j+1);
+                    j++;
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
