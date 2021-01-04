8c24983dee4df4108bc40eb69ba807ffcc6c7096
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-04 10:33:37 +0100

    Milestone 3

---
+++ M2 Project/Milestone 3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Milestone_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 0;
+            int minutes = 0;
+            int seconds = 0;
+            bool firstLoop = true;
+
+            while(true)
+            {
+                hour = DateTime.Now.Hour;
+                minutes = DateTime.Now.Minute;
+                seconds = DateTime.Now.Second;
+
+                while (hour < 24)
+                {
+                    if(!firstLoop)
+                    { 
+                        minutes = 0;
+                         
+                     }
+                while (minutes < 60)
+                    {
+                        if (!firstLoop)
+                        {
+                            seconds = 0;
+                            
+                        }
+                            
+                        while(seconds < 60)
+                        {
+                            Console.WriteLine("{0}:{1}:{2}", hour.ToString("D2"), minutes.ToString("D2"), seconds.ToString("D2"));
+                            Thread.Sleep(1000);
+                            seconds++;
+                            firstLoop = false;
+                        }
+                        minutes++;
+                    }
+
+                    hour++;
+                }
+
+                Console.WriteLine("{0}:{1}:{2}", hour.ToString("D2"), minutes.ToString("D2"), seconds.ToString("D2"));
+                Thread.Sleep(1000);
+
+                //Sumar hora
+
+            }
+        }
+    }
+}
