275e5e5e0f2fbbbec41886574808672a6b7cd028
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-04 22:11:00 +0100

    Milestone 3 and 2

---
+++ M3 project/Milestone 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Milestone_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcularNotaMitjana();
+
+        }
+
+        public static void CalcularNotaMitjana()
+        {
+            //Notas alumnos
+            int[,] notasAlumnes = new int[5, 3];
+
+            int i = 0;
+
+            while ( i < 5)
+            {          
+                Console.WriteLine("Introduce 3 marks of the student number {0}: ", i+1);
+                notasAlumnes[i,0]= Convert.ToInt32(Console.ReadLine());
+                notasAlumnes[i, 1] = Convert.ToInt32(Console.ReadLine());
+                notasAlumnes[i, 2] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+            i = 0;
+            while ( i < 5)
+            {
+
+                double notaMitjana = (notasAlumnes[i, 0] + notasAlumnes[i, 1] + notasAlumnes[i, 2]) / 3;
+
+                if (notaMitjana >= 5)
+                {
+                    Console.WriteLine("L'alumne número {0} amb nota mitja {1:R} ha APROBAT", i+1, notaMitjana);
+                }
+                else
+                {
+                    Console.WriteLine("L'alumne número {0} amb nota mitja {1:R} ha SUSPÈS", i + 1, notaMitjana);
+                }
+
+                i++;
+            }
+        }
+    }
+}
---
+++ M3 project/Milestone 3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Milestone_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Introduce how many fibonacci numbers do you want to calculate: ");
+
+             int n = Convert.ToInt32(Console.ReadLine());
+
+             Fibonacci(n);
+
+            //int a, b, limite, i, auxiliar;
+            //Console.WriteLine("Ingrese el número de numeros de Fibonacci que desea mostrar");
+            //limite = int.Parse(Console.ReadLine());
+            //a = 0;
+            //b = 1;
+            //for (i = 0; i < limite; i++)
+            //{
+            //    auxiliar = a;
+            //    a = b;
+            //    b = auxiliar + a;
+            //    Console.WriteLine(a);
+            //}
+        }
+
+        public static void Fibonacci(int n)
+        {
+            int result = 0;
+            int result1 = 1;
+            int result2;
+
+            int i = 0;
+
+            while (i < n)
+            { 
+                result2 = result;
+                result = result1 ;
+                result1 = result + result2;
+               
+
+                Console.WriteLine("{0}", result);
+                i++;
+            }
+        }
+    }
+}
