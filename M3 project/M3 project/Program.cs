503d623e7a308d0504902c689908892c6fd0d28a
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-04 12:42:32 +0100

    Fase 3

--- M3 project/M3 project/Program.cs
+++ M3 project/M3 project/Program.cs
@@ -19,6 +19,7 @@ namespace M3_project
             string ciutat5 = "";
             string ciutat6 = "";
             string[] arrayCiutats = new string[6];
+            string[] arrayCiutatsModificades = new string[6];
 
             Console.WriteLine("Introdueix el nom de 6 ciutats:");
             ciutat1 = Console.ReadLine();
@@ -38,8 +39,16 @@ namespace M3_project
             Comparison<string> comparador = new Comparison<string>((firstString, secondString) => firstString.CompareTo(secondString));
             Array.Sort(arrayCiutats, comparador);
 
+            int i = 0;
+            foreach (string cadena in arrayCiutats)
+            {
+               arrayCiutatsModificades[i] =  cadena.Replace('a', '4');
+                i++;
+            }
+
 
             Console.WriteLine("Les 6 ciutats ordenades alfabeticament son: {0}, {1}, {2}, {3}, {4} i {5}.", arrayCiutats[0], arrayCiutats[1], arrayCiutats[2], arrayCiutats[3], arrayCiutats[4], arrayCiutats[5]);
+            Console.WriteLine("Les 6 ciutats ordenades alfabeticament son: {0}, {1}, {2}, {3}, {4} i {5}.", arrayCiutatsModificades[0], arrayCiutatsModificades[1], arrayCiutatsModificades[2], arrayCiutatsModificades[3], arrayCiutatsModificades[4], arrayCiutatsModificades[5]);
         }
     }
 }
